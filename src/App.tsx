6777b5dc6d32244ba394bef77a78c75e2ddde394
Ipixels02 <ar4iking19@gmail.com>
2022-10-18 20:38:38 +0500

    Добавлено: список пациентов Добавление пациента Назначение консультации Поиск по пациентам

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useMemo, useState} from 'react';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import ListUsers from "./Component/ListUsers";
+import CreatePatient from "./Component/CreatePatient";
+import data from "./data/db.json"
+import MyButton from "./UI/Button/MyButton";
+import MyModal from "./UI/Modal/MyModal";
+import MyInput from "./UI/Input/MyInput";
+import CreateConsultation from "./Component/CreateConsultation";
 
 function App() {
+
+    const [users, setUsers] = useState<any>(data.users); //Получаем список пациентов
+    const [modal,setModal] = useState(false);
+    const [searchQuery, setSearchQuery] = useState('')
+
+
+    const searchedUsers = useMemo(() => {
+        return users.filter((user: any) => Object.keys(user).some((prop) => {
+                return (user[prop] + "").toLowerCase().includes(searchQuery?.toLowerCase())
+        }))
+    }, [searchQuery])
+
+    const createPatient = (newPatient: any) => {
+        setUsers([...users, newPatient])
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 style={{textAlign: "center", paddingBottom: 50}}>Консультации для пациентов</h1>
+        <MyInput
+            value={searchQuery}
+            onChange={(e: any) => setSearchQuery(e.target.value)}
+            placeholder={"Поиск..."}
+        />
+        <ListUsers users={searchedUsers}/>
+
+        <MyButton style={{marginTop: 40}} onClick={() => setModal(true)}>
+            Создать пользователя
+        </MyButton>
+        <MyModal visible={modal} setVisible={setModal}>
+            <CreatePatient create={createPatient}/>
+        </MyModal>
+
+        <MyButton style={{margin: 40}} onClick={() => setModal(true)}>
+            Назначить консультацию
+        </MyButton>
+        {/*<MyModal visible={modal} setVisible={setModal}>*/}
+        {/*    <CreateConsultation create={createPatient}/>*/}
+        {/*</MyModal>*/}
     </div>
   );
 }
---
+++ src/UI/Modal/MyModal.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import cl from './MyModule.module.css'
+
+const MyModal = ({children, visible, setVisible}:any) => {
+
+    const rootClasses = [cl.myModal]
+    if (visible) {
+        rootClasses.push(cl.active)
+    }
+
+    return (
+        <div className={rootClasses.join(' ')} onClick={() => setVisible(false)}>
+            <div className={cl.myModalContent} onClick={(e): any => e.stopPropagation()}>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default MyModal;
